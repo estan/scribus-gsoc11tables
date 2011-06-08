2d578987b23cae9a598fa4f5e960191efb065473
Elvis Stansvik <elvstone@gmail.com>
2011-06-08 15:54:08 +0200

    Use inspection to figure out test methods.
    Adding a new test is now just a matter of adding a new method to
    TableTests.

--- Scribus/scribus/tests/scripts/test_tables.py
+++ Scribus/scribus/tests/scripts/test_tables.py
@@ -10,11 +10,8 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 
-Instructions for adding a new test:
-
-  1) Write the test method.
-  2) Add the method to the 'tests' list.
-  3) Done.
+To add a new test, simply add a new test method named 'test_mytest' to the
+TableTests class, and the test will be runned automatically.
 
 Use check() to check a condition and fail(msg) to manually fail a test. The
 tests are runned in a "fail fast" fashion; on failure, the test method will
@@ -24,53 +21,48 @@ stop executing and testing move on to the next test method.
 from scribus import *
 from traceback import print_exc
 from sys import stdout
+from inspect import getmembers, ismethod
 
-""" Test for createTable(...) """
-def test_construction():
-    # Test valid construction.
-    table = createTable(50, 50, 90, 90, 3, 3)
-    check(objectExists(table))
-    check(getTableColumns(table) == 3)
-    check(getTableRows(table) == 3)
-    check(getTableColumnWidth(0, table) == 30)
-    check(getTableColumnWidth(1, table) == 30)
-    check(getTableColumnWidth(2, table) == 30)
-    check(getTableRowHeight(0, table) == 30)
-    check(getTableRowHeight(1, table) == 30)
-    check(getTableRowHeight(2, table) == 30)
-    deleteObject(table)
+class TableTests:
+    """ Tests for tables """
+    def test_construction(self):
+        """ Test for createTable(...) """
+        # Test valid construction.
+        table = createTable(50, 50, 90, 90, 3, 3)
+        check(objectExists(table))
+        check(getTableColumns(table) == 3)
+        check(getTableRows(table) == 3)
+        check(getTableColumnWidth(0, table) == 30)
+        check(getTableColumnWidth(1, table) == 30)
+        check(getTableColumnWidth(2, table) == 30)
+        check(getTableRowHeight(0, table) == 30)
+        check(getTableRowHeight(1, table) == 30)
+        check(getTableRowHeight(2, table) == 30)
+        deleteObject(table)
 
-    # Test invalid construction.
-    bad_args = [(-1, 3), (3, -1), (0, 3), (3, 0)]
-    for args in bad_args:
-        try:
-            table = createTable(50, 50, 90, 90, args[0], args[1]);
-            fail('Expected ValueError, numRows=%i, numColumns=%i' % (args[0], args[1]))
-        except ValueError:
-            pass # Expected.
-        finally:
-            if objectExists(table):
-                deleteObject(table)
+        # Test invalid construction.
+        bad_args = [(-1, 3), (3, -1), (0, 3), (3, 0)]
+        for args in bad_args:
+            try:
+                table = createTable(50, 50, 90, 90, args[0], args[1]);
+                fail('Expected ValueError, numRows=%i, numColumns=%i' % (args[0], args[1]))
+            except ValueError:
+                pass # Expected.
+            finally:
+                if objectExists(table):
+                    deleteObject(table)
 
-""" Test for setTableRowHeight(...) """
-def test_set_row_height():
-    pass # TODO
+    def test_set_row_height(self):
+        """ Test for setTableRowHeight(...) """
+        pass # TODO
 
-""" Test for setTableColumnWidth(...) """
-def test_set_column_width():
-    pass # TODO
+    def test_set_column_width(self):
+        """ Test for setTableColumnWidth(...) """
+        pass # TODO
 
-""" Test for mergeTableCells(...) """
-def test_merge_cells():
-    pass # TODO
-
-""" List of test methods """
-tests = [
-    test_construction,
-    test_set_row_height,
-    test_set_column_width,
-    test_merge_cells
-]
+    def test_merge_cells(self):
+        """ Test for mergeTableCells(...) """
+        pass # TODO
 
 #
 # Test "framework" code below.
@@ -91,14 +83,20 @@ def fail(msg):
     """ Fails test with msg """
     raise TestFailure(msg);
 
+def is_test_method(obj):
+    """ Returns True if obj is a test method """
+    return ismethod(obj) and obj.__name__.startswith('test_')
+
 if __name__ == '__main__':
     print 'Running table tests...'
-    ntests = len(tests)
+    tests = TableTests()
+    methods = getmembers(tests, is_test_method)
+    ntests = len(methods)
     nfailed = 0
-    for testnr, test in enumerate(tests):
-        print '\t%i/%i: %s()%s' % (testnr + 1, ntests, test.__name__, '.' * (30 - len(test.__name__))),
+    for testnr, (name, method) in enumerate(methods):
+        print '\t%i/%i: %s()%s' % (testnr + 1, ntests, name, '.' * (30 - len(name))),
         try:
-            test()
+            method()
         except:
             print 'Failed'
             print_exc(file=stdout)
