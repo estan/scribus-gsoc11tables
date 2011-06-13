1ff2a9dcd6fe9e669051ed6dd8fb52896a277d58
Elvis Stansvik <elvstone@gmail.com>
2011-06-13 22:12:14 +0200

    Add copyright.

--- Scribus/scribus/ui/smtablestyle.h
+++ Scribus/scribus/ui/smtablestyle.h
@@ -1,4 +1,6 @@
 /*
+Copyright (C) 2011 Elvis Stansvik <elvstone@gmail.com>
+
 For general Scribus (>=1.3.2) copyright and licensing information please refer
 to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
