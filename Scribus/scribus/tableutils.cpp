a12c5767e3e81e69728a56b1f287872a5a6accc2
Elvis Stansvik <elvstone@gmail.com>
2011-07-06 12:27:35 +0200

    Joining algorithm now 100% correct.
    There is actually 43 cases, I had forgot two. And after sitting
    down and strictly rewriting the algorithm, I am now convinced
    it is 100% correct and covers all cases. The code is also much
    clearer now.
    
    Any bugs still left in border painting is definitely in the
    painting methods.

--- Scribus/scribus/tableutils.cpp
+++ Scribus/scribus/tableutils.cpp
@@ -81,61 +81,109 @@ void joinVertical(const TableBorder& border, const TableBorder& topLeft, const T
 	endOffsetFactors->setY(0.0);
 
 	/*
-	 * The numbered cases in the code below refers to the 41 possible join cases illustrated
+	 * The numbered cases in the code below refers to the 43 possible join cases illustrated
 	 * in the picture at http://wiki.scribus.net/canvas/File:Table_border_join_cases.png
 	 */
 
 	/*
-	 * Adjust start point(s).
+	 * Adjust start point(s). Possible cases are 1-20, 26-39.
 	 */
-	if (top.isNull() && !border.joinsWith(topLeft) && !border.joinsWith(topRight))
+	if (border.joinsWith(topLeft))
 	{
-		// Cases: 14, 18, 35, 37.
-		start->setY(start->y() - 0.5 * qMax(topLeft.width(), topRight.width()));
+		if (border.joinsWith(topRight))
+		{
+			if (!border.joinsWith(top))
+			{
+				// Cases: 8, 19.
+				startOffsetFactors->setY(-0.5);
+			}
+		}
+		else if (!border.joinsWith(top))
+		{
+			if (top.joinsWith(topRight))
+			{
+				// Cases: 15.
+				start->setY(start->y() + 0.5 * topRight.width());
+			}
+			else
+			{
+				// Cases: 5, 17, 27, 38.
+				startOffsetFactors->setY(-0.5);
+			}
+		}
 	}
-	else if (!border.joinsWith(topLeft) && !border.joinsWith(topRight) && !border.joinsWith(top))
+	else if (border.joinsWith(topRight))
 	{
-		// Cases: 1, 2, 3, 6, 12, 24, 26, 29, 31.
+		if (!border.joinsWith(top))
+		{
+			if (top.joinsWith(topLeft))
+			{
+				// Cases: 14.
+				start->setY(start->y() + 0.5 * topLeft.width());
+			}
+			else
+			{
+				// Cases: 4, 18, 32, 36.
+				startOffsetFactors->setY(-0.5);
+			}
+		}
+	}
+	else if (border.joinsWith(top))
+	{
+		if (topLeft.joinsWith(topRight))
+		{
+			// Cases: 11.
+			start->setY(start->y() + 0.5 * topLeft.width());
+		}
+	}
+	else
+	{
+		// Cases: 1, 2, 3, 6, 12, 16, 20, 26, 28, 31, 33, 37, 39.
 		start->setY(start->y() + 0.5 * qMax(topLeft.width(), topRight.width()));
 	}
-	else if (!border.joinsWith(top) && (border.joinsWith(topLeft) || border.joinsWith(topRight)))
-	{
-		// Cases: 4, 5, 8, 15, 16, 17, 25, 30, 34, 36.
-		startOffsetFactors->setY(-0.5);
-	}
-	else if (topLeft.joinsWith(topRight) && !topLeft.joinsWith(top) && !topRight.joinsWith(top))
-	{
-		// Cases: 11.
-		start->setY(start->y() + 0.5 * qMax(topLeft.width(), topRight.width()));
-	}
-	// Cases: 7, 9, 10, 13, 27, 28, 32, 33 - No adjustment to start point needed.
-	// Cases: 19, 20, 21, 22, 23, 38, 39, 40, 41 - N/A.
+	// Cases: 7, 9, 10, 13, 29, 30, 34, 35 - No adjustment to start point(s) needed.
 
 	/*
-	 * Adjust end point(s).
+	 * Adjust end point(s). Possible cases are 1-15, 21-35, 40-43.
 	 */
-	if (bottom.isNull() && !border.joinsWith(bottomLeft) && !border.joinsWith(bottomRight))
+	if (border.joinsWith(bottomLeft))
 	{
-		// Cases: 19, 23, 39, 41.
-		end->setY(end->y() + 0.5 * qMax(bottomLeft.width(), bottomRight.width()));
+		if (border.joinsWith(bottomRight))
+		{
+			if (!border.joinsWith(bottom))
+			{
+				// Cases: 6, 24.
+				endOffsetFactors->setY(0.5);
+			}
+		}
+		else if (!border.joinsWith(bottom))
+		{
+			// Cases: 2, 14, 22, 28, 42.
+			endOffsetFactors->setY(0.5);
+		}
 	}
-	else if (!border.joinsWith(bottomLeft) && !border.joinsWith(bottomRight) && !border.joinsWith(bottom))
+	else if (border.joinsWith(bottomRight))
 	{
-		// Cases: 1, 4, 5, 8, 12, 24, 25, 29, 30.
+		if (!border.joinsWith(bottom))
+		{
+			// Cases: 3, 15, 23, 33, 40.
+			endOffsetFactors->setY(0.5);
+		}
+	}
+	else if (border.joinsWith(bottom))
+	{
+		if (bottomLeft.joinsWith(bottomRight))
+		{
+			// Cases: 11.
+			end->setY(end->y() - 0.5 * bottomLeft.width());
+		}
+	}
+	else
+	{
+		// Cases: 1, 4, 5, 8, 12, 21, 25, 26, 27, 31, 32, 41, 43.
 		end->setY(end->y() - 0.5 * qMax(bottomLeft.width(), bottomRight.width()));
 	}
-	else if (!border.joinsWith(bottom) && (border.joinsWith(bottomLeft) || border.joinsWith(bottomRight)))
-	{
-		// Cases: 2, 3, 6, 20, 21, 22, 26, 31, 38, 40.
-		endOffsetFactors->setY(0.5);
-	}
-	else if (bottomLeft.joinsWith(bottomRight) && !bottomLeft.joinsWith(bottom) && !bottomRight.joinsWith(bottom))
-	{
-		// Cases: 11.
-		end->setY(end->y() - 0.5 * qMax(bottomLeft.width(), bottomRight.width()));
-	}
-	// Cases: 7, 9, 10, 13, 27, 28, 32, 33 - No adjustment to end point needed.
-	// Cases: 14, 15, 16, 17, 18, 34, 35, 36, 37 - N/A.
+	// Cases: 7, 9, 10, 13, 29, 30, 34, 35 - No adjustment to end point(s) needed.
 }
 
 void joinHorizontal(const TableBorder& border, const TableBorder& topLeft, const TableBorder& left,
@@ -155,97 +203,135 @@ void joinHorizontal(const TableBorder& border, const TableBorder& topLeft, const
 	endOffsetFactors->setY(0.0);
 
 	/*
-	 * The numbered cases in the code below refers to the 41 possible join cases illustrated
+	 * The numbered cases in the code below refers to the 43 possible join cases illustrated
 	 * in the picture at http://wiki.scribus.net/canvas/File:Table_border_join_cases.png
 	 */
 
 	/*
-	 * Adjust start point.
+	 * Adjust start point(s). Possible cases are 1-25, 31-37, 40-41.
 	 */
-	if (left.isNull() && (topLeft.isNull() || bottomLeft.isNull()))
+	if (border.joinsWith(bottomLeft))
 	{
-		if (border.joinsWith(topLeft) || border.joinsWith(bottomLeft))
+		if (border.joinsWith(topLeft))
 		{
-			// Cases: 34, 38.
+			if (border.joinsWith(left))
+			{
+				// Cases: 10.
+				startOffsetFactors->setX(0.5);
+			}
+			else
+			{
+				// Cases: 7, 34.
+				startOffsetFactors->setX(0.5);
+			}
+		}
+		else
+		{
+			if (border.joinsWith(left))
+			{
+				// Cases: 8, 19.
+				startOffsetFactors->setX(0.5);
+			}
+			else if (left.joinsWith(topLeft))
+			{
+				// Cases: 14.
+				start->setX(start->x() + 0.5 * topLeft.width());
+			}
+			else
+			{
+				// Cases: 4, 18, 32, 36.
+				startOffsetFactors->setX(0.5);
+			}
+		}
+	}
+	else if (border.joinsWith(topLeft))
+	{
+		if (border.joinsWith(left))
+		{
+			// Cases: 6, 24.
 			startOffsetFactors->setX(0.5);
 		}
 		else
 		{
-			// Cases: 35, 39.
-			start->setX(start->x() + 0.5 * qMax(topLeft.width(), bottomLeft.width()));
+			// Cases: 3, 15, 23, 33, 40.
+			startOffsetFactors->setX(0.5);
 		}
 	}
-	else if (left.isNull() && !topLeft.isNull() && !bottomLeft.isNull() &&
-			!border.joinsWith(topLeft) && !border.joinsWith(bottomLeft))
+	else if (!border.joinsWith(left) &&
+			 (topLeft.joinsWith(bottomLeft) || topLeft.joinsWith(left) || bottomLeft.joinsWith(left)))
 	{
-		if (topLeft.joinsWith(bottomLeft))
-		{
-			// Cases: 33.
-			start->setX(start->x() + 0.5 * qMax(topLeft.width(), bottomLeft.width()));
-		}
-		else
-		{
-			// Cases: 29.
-			start->setX(start->x() - 0.5 * qMax(topLeft.width(), bottomLeft.width()));
-		}
-	}
-	else if ((!border.joinsWith(topLeft) && !border.joinsWith(bottomLeft) && !border.joinsWith(left)) &&
-			(topLeft.joinsWith(left) || bottomLeft.joinsWith(left) || topLeft.joinsWith(bottomLeft)))
-	{
-		// Cases: 2, 5, 9, 13, 15, 20.
+		// Cases: 2, 5, 9, 13, 17, 22, 35.
 		start->setX(start->x() + 0.5 * qMax(topLeft.width(), bottomLeft.width()));
 	}
-	else if (border.joinsWith(topLeft) || border.joinsWith(bottomLeft))
+	else if (left.isNull())
 	{
-		// Cases: 3, 4, 6, 7, 8, 10, 16, 17, 21, 22, 30, 31, 32.
-		startOffsetFactors->setX(0.5);
+		// Cases: 31, 37, 41.
+		start->setX(start->x() - 0.5 * qMax(topLeft.width(), bottomLeft.width()));
 	}
-	// Cases: 1, 11, 12, 14, 18, 19, 23 - No adjustment to start point needed.
-	// Cases: 24, 25, 26, 27, 28, 36, 37, 40, 41 - N/A.
+	// Cases: 1, 11, 12, 16, 20, 21, 25 - No adjustment to start point(s) needed.
 
 	/*
-	 * Adjust end point.
+	 * Adjust end point(s). Possible cases are 1-30, 38-39, 42-43.
 	 */
-	if (right.isNull() && (topRight.isNull() || bottomRight.isNull()))
+	if (border.joinsWith(bottomRight))
 	{
-		if (border.joinsWith(topRight) || border.joinsWith(bottomRight))
+		if (border.joinsWith(topRight))
 		{
-			// Cases: 36, 40.
+			if (border.joinsWith(right))
+			{
+				// Cases: 10.
+				endOffsetFactors->setX(-0.5);
+			}
+			else
+			{
+				// Cases: 9, 29.
+				endOffsetFactors->setX(-0.5);
+			}
+		}
+		else if (border.joinsWith(right))
+		{
+			// Cases: 6, 24.
 			endOffsetFactors->setX(-0.5);
 		}
 		else
 		{
-			// Cases: 37, 41.
-			end->setX(end->x() - 0.5 * qMax(topRight.width(), bottomRight.width()));
+			if (right.joinsWith(topRight))
+			{
+				// Cases: 15.
+				end->setX(end->x() - 0.5 * topRight.width());
+			}
+			else
+			{
+				// Cases: 5, 17, 27, 38.
+				endOffsetFactors->setX(-0.5);
+			}
 		}
 	}
-	else if (right.isNull() && !topRight.isNull() && !bottomRight.isNull() &&
-			!border.joinsWith(topRight) && !border.joinsWith(bottomRight))
+	else if (border.joinsWith(topRight))
 	{
-		if (topRight.joinsWith(bottomRight))
+		if (border.joinsWith(right))
 		{
-			// Cases: 28.
-			end->setX(end->x() - 0.5 * qMax(topRight.width(), bottomRight.width()));
+			// Cases: 6, 24.
+			endOffsetFactors->setX(-0.5);
 		}
 		else
 		{
-			// Cases: 24.
-			end->setX(end->x() - 0.5 * qMax(topRight.width(), bottomRight.width()));
+			// Cases: 2, 14, 22, 28, 42.
+			endOffsetFactors->setX(-0.5);
 		}
 	}
-	else if ((!border.joinsWith(topRight) && !border.joinsWith(bottomRight) && !border.joinsWith(right)) &&
-			 (topRight.joinsWith(right) || bottomRight.joinsWith(right) || topRight.joinsWith(bottomRight)))
+	else if (!border.joinsWith(right) &&
+			 (topRight.joinsWith(bottomRight) || topRight.joinsWith(right) || bottomRight.joinsWith(right)))
 	{
-		// Cases: 3, 4, 7, 13, 16, 21, 28.
+		// Cases: 3, 4, 7, 13, 18, 23, 30.
 		end->setX(end->x() - 0.5 * qMax(topRight.width(), bottomRight.width()));
 	}
-	else if (border.joinsWith(topRight) || border.joinsWith(bottomRight))
+	else if (right.isNull())
 	{
-		// Cases: 2, 5, 6, 8, 9, 10, 15, 17, 20, 22, 25, 26, 27.
-		endOffsetFactors->setX(-0.5);
+		// Cases: 26, 39, 43.
+		end->setX(end->x() + 0.5 * qMax(topRight.width(), bottomRight.width()));
 	}
-	// Cases: 1, 11, 12, 14, 18, 19, 23 - No adjustment to end point needed.
-	// Cases: 29, 30, 31, 32, 33, 34, 35, 38, 39 - N/A.
+	// Cases: 1, 11, 12, 16, 20, 21, 25 - No adjustment to end point(s) needed.
 }
 
 } // namespace TableUtils
