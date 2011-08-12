5226d8c60207949bf243445f9b7957cec88a8539
Elvis Stansvik <elvstone@gmail.com>
2011-08-12 07:13:05 +0200

    Handle activation of cells and typing of text into cells:
    Cells are currently activated by clicking on them with the mouse.
    
    The typing into cells is handled by passing on the event to the
    handleModeEditKey(...) of the text frame of the currently active cell.
    
    Cell selections are now temporarily disabled while I work on cursor
    painting, text selection et.c. It will be brought back later.
    
    NOTE: In order to get canvas updates I had to make CanvasMode_EditTable
          a friend of Canvas and set m_canvas->m_viewMode.forceRedraw = true
          before the call to m_canvas->update(). Will have to investigate if
          there's a better way.

--- Scribus/scribus/canvas.h
+++ Scribus/scribus/canvas.h
@@ -95,6 +95,7 @@ public:
 	friend class CanvasMode_EditMeshGradient;
 	friend class CanvasMode_EditPolygon;
 	friend class CanvasMode_EditSpiral;
+	friend class CanvasMode_EditTable;
 	friend class CanvasMode_EyeDropper;
 	friend class CanvasMode_FrameLinks;
 	friend class CanvasMode_Magnifier;
--- Scribus/scribus/canvasmode_edittable.cpp
+++ Scribus/scribus/canvasmode_edittable.cpp
@@ -69,7 +69,13 @@ void CanvasMode_EditTable::keyPressEvent(QKeyEvent* event)
 	{
 		// Go back to normal mode.
 		m_view->requestMode(modeNormal);
-		return;
+	}
+	else {
+		// Pass event to text frame of active cell.
+		bool repeat;
+		m_table->activeCell().textFrame()->handleModeEditKey(event, repeat);
+		m_canvas->m_viewMode.forceRedraw = true; // Hack?
+		m_canvas->update(m_canvas->canvasToLocal(m_table->getBoundingRect()));
 	}
 }
 
@@ -117,36 +123,44 @@ void CanvasMode_EditTable::mousePressEvent(QMouseEvent* event)
 	qreal threshold = m_doc->guidesPrefs().grabRadius / m_canvas->scale();
 	TableHandle handle = m_table->hitTest(canvasPoint, threshold);
 
-	switch (handle.type())
+	if (event->button() == Qt::LeftButton)
 	{
-		case TableHandle::RowSelect:
-			// Not implemented.
-			break;
-		case TableHandle::RowResize:
-			m_rowResizeGesture->setup(m_table, handle.index());
-			m_view->startGesture(m_rowResizeGesture);
-			break;
-		case TableHandle::ColumnSelect:
-			// Not implemented.
-			break;
-		case TableHandle::ColumnResize:
-			m_columnResizeGesture->setup(m_table, handle.index());
-			m_view->startGesture(m_columnResizeGesture);
-			break;
-		case TableHandle::TableResize:
-			m_tableResizeGesture->setup(m_table);
-			m_view->startGesture(m_tableResizeGesture);
-			break;
-		case TableHandle::CellSelect:
-			m_cellSelectGesture->setup(m_table, m_table->cellAt(canvasPoint));
-			m_view->startGesture(m_cellSelectGesture);
-			break;
-		case TableHandle::None:
-			// Not implemented.
-			break;
-		default:
-			qWarning("Unknown hit target");
-			break;
+		switch (handle.type())
+		{
+			case TableHandle::RowSelect:
+				// Not implemented.
+				break;
+			case TableHandle::RowResize:
+				m_rowResizeGesture->setup(m_table, handle.index());
+				m_view->startGesture(m_rowResizeGesture);
+				break;
+			case TableHandle::ColumnSelect:
+				// Not implemented.
+				break;
+			case TableHandle::ColumnResize:
+				m_columnResizeGesture->setup(m_table, handle.index());
+				m_view->startGesture(m_columnResizeGesture);
+				break;
+			case TableHandle::TableResize:
+				m_tableResizeGesture->setup(m_table);
+				m_view->startGesture(m_tableResizeGesture);
+				break;
+			case TableHandle::CellSelect:
+				//m_cellSelectGesture->setup(m_table, m_table->cellAt(canvasPoint));
+				//m_view->startGesture(m_cellSelectGesture);
+				m_table->setActiveCell(m_table->cellAt(canvasPoint));
+				break;
+			case TableHandle::None:
+				// Not implemented.
+				break;
+			default:
+				qWarning("Unknown hit target");
+				break;
+		}
+	}
+	else if (event->button() == Qt::RightButton)
+	{
+		qDebug() << "Show table context menu";
 	}
 }
 
